794892d20add6b44449c5eb2d18e227284bb835b
Ehsan AlemMohammadGhasemlou <e.ghasemloo@gmail.com>
2018-10-19 02:08:19 +0330

    Fix "cd %~dp0" with appveyor

--- compile.bat
+++ compile.bat
@@ -1,6 +1,5 @@
 set QTDIR=C:\Qt\5.11.1\mingw53_32
 set PATH=%QTDIR%\bin;C:\Qt\Tools\mingw530_32\bin;C:\Qt\Tools\QtCreator\bin;%PATH%
-cd %~dp0
 
 qmake.exe PersianSubtitleFixer.pro -spec win32-g++ "CONFIG+=qtquickcompiler"
 mingw32-make.exe 
