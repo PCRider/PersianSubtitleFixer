85c67e6a5404f24bd02f853d615f49a3777ce556
Ehsan AlemMohammadGhasemlou <e.ghasemloo@gmail.com>
2018-10-19 02:38:35 +0330

    Update build script to remove source file from deploy

--- compile.bat
+++ compile.bat
@@ -4,7 +4,16 @@ set PATH=%QTDIR%\bin;C:\Qt\Tools\mingw530_32\bin;C:\Qt\Tools\QtCreator\bin;%PATH
 qmake.exe PersianSubtitleFixer.pro -spec win32-g++ "CONFIG+=qtquickcompiler"
 mingw32-make.exe 
 windeployqt --qmldir . ./release/PersianSubtitleFixer.exe
-
+cd release
+del *.o
+del *.h
+del *.cpp
+cd ..
 qmake.exe PersianSubtitleFixer.pro -spec win32-g++ "CONFIG+=debug" "CONFIG+=qml_debug"
 mingw32-make.exe 
 windeployqt --qmldir . ./debug/PersianSubtitleFixer.exe
+cd debug
+del *.o
+del *.h
+del *.cpp
+cd ..
